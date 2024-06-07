89529082cd1152cb2e1dac4a7f032bd134da8c7a
GinoZampa <ginojesus2002@gmail.com>
2024-06-07 01:10:41 -0300

    TP finalizado

--- TP_2.1/gcl/__init__.py
+++ TP_2.1/gcl/__init__.py
@@ -1,61 +1,14 @@
-import time
-import os
+class GCL:
+    def __init__(self, seed, a, c, m):
+        self.seed = seed
+        self.a = a
+        self.c = c
+        self.m = m
+        self.current = seed
 
-class  GeneradorLinealCongruencialMixto:
-  def __init__(self, seed: int = int(os.getpid() + time.time()), 
-               a: int =1664525, c: int =1013904223, m: int=2**32):
-    print(seed)
-    self.ciclos = 0 
-    self.valores = []
-    self.parametros(seed, a, c, m)
+    def next(self):
+        self.current = (self.a * self.current + self.c) % self.m
+        return self.current / self.m
 
-  def parametros(self, seed, a, c, m):
-    #se necesita primero m para poder validar los otros parametros
-    self.set_m(m)
-    self.set_seed(seed)
-    self.set_a(a)
-    self.set_c(c)    
-  
-  def next(self):
-    self.seed = (self.a * self.seed + self.c) % self.m
-    self.ciclos += 1
-    self.valores.append([self.ciclos, self.seed])
-    return self.seed
-  
-  def next_float(self):
-    return self.next() / self.m
-  
-  def next_float_range(self, min, max):
-    if min >= max:
-      raise ValueError("min debe ser menor a max")
-    return min + (max - min) * self.next_float()
-  
-  def next_int_range(self, min, max):
-    if min >= max:
-      raise ValueError("min debe ser menor a max")
-    return int(min + (max - min) * self.next_float())
-  
-  def set_seed(self, seed):
-    if seed < 0 or seed >= self.m or seed == None:
-      raise ValueError("Rango de seed: [0,m)")
-    self.seed = seed
-    self.first_seed = self.seed
-  def set_a(self, a):
-    if  a < 0 or a >= self.m or a == None:
-      raise ValueError("Rango de a [0,m)")
-    self.a = a
-  def set_c(self, c):
-    if c > self.m or c == None:
-      raise ValueError("Rango de c (inf,m]")
-    self.c = c
-  def set_m(self, m):
-    if m <= 0 or m == None:
-      raise ValueError("Rango de m (0,inf)")
-    self.m = m
-  
-  def __repr__(self):
-    return "GLC: a={} c={} m={} seed={} \nciclo={} number={}".format(self.a, self.c, self.m, self.first_seed, self.ciclos, self.seed)
-
-  def __str__(self):
-    return "ciclo={} number={}".format(self.ciclos, self.seed)
-  
+    def generar_secuencia(self, n):
+        return [self.next() for _ in range(n)]
--- TP_2.1/main.py
+++ TP_2.1/main.py
@@ -1,30 +1,88 @@
-from gcl import GeneradorLinealCongruencialMixto as randu_generator
-from mid_square import middleSquareGenerator as mid_square_generator
+from gcl import GCL
+from mid_square import MediosCuadrados
+import numpy as np
+from scipy.stats import kstest
+import time
+from scipy.stats import chi2
+import matplotlib.pyplot as plt
+
+def genCL(n, a=1103515245, c=12345, m=2**31):
+    seed = int(time.time())  # Usar el tiempo actual como semilla
+    gcl = GCL(seed, a, c, m)
+    return gcl.generar_secuencia(n)
+
+def genCuad(n):
+    seed = int(time.time())  # Usar el tiempo actual como semilla
+    digitos=4
+    ms_generator = MediosCuadrados(seed, digitos)
+    return ms_generator.generar_secuencia(n) 
+
+def test_media(secuencia):
+    n = len(secuencia)
+    media = np.mean(secuencia)
+    media_esp = 0.5
+    media_test = abs(media - media_esp) < (1 / np.sqrt(12 * n))
+    print("Media: ", media)
+    print("Media esperada: ", media_esp)
+    print("Paso el test de media: ", media_test)
+
+def test_chi_cuadrado(secuencia, k=10):
+    n = len(secuencia)
+    frecuencia_esp = n / k
+    frecuencia_obs, _ = np.histogram(secuencia, bins=k, range=(0.0, 1.0))
+    chi_cuadrado = np.sum((frecuencia_obs - frecuencia_esp)**2 / frecuencia_obs)
+    valor_critico = chi2.ppf(0.95, df=k-1)
+    pasa_test = chi_cuadrado < valor_critico
+    print("Estadistico: ", chi_cuadrado)
+    print("Valor critico: ", valor_critico)
+    print("Paso es test de Chi-cuadrado: ", pasa_test)
+
+def test_autocorrelacion(secuencia, lag=1):
+    n = len(secuencia)
+    media = np.mean(secuencia)
+    varianza = np.var(secuencia)
+    if varianza == 0:
+        print("Varianza =  0, no se puede desarrollar el test de autocorrelación")
+        return False
+    autocovarianza = np.mean([(secuencia[i] - media) * (secuencia[i + lag] - media) for i in range(n - lag)])
+    autocorrelacion = autocovarianza / varianza
+    intervalo_confianza = 1.96 / np.sqrt(n)
+    pasa_test = abs(autocorrelacion) < intervalo_confianza
+    print("Autocorrelacion (lag ", lag,"): ", autocorrelacion)
+    print("95% Intervalo de confianza: ", intervalo_confianza)
+    print("Paso el test de la Autocorrelacion: ", pasa_test)
 
 
-def main():
-  randu()
-  #mid_square()
+def test_kolmogorov_smirnov(secuencia):
+    estadistico, valor_p = kstest(secuencia, 'uniforme')
+    print("Estadistico Kolmogorov-Smirnov: ", estadistico)
+    print("Valor_p: ", valor_p)
+    alpha = 0.05  # Nivel de significancia del 5%
+    pasa_test = valor_p > alpha
+    print("Paso el test de Kolmogorov-Smirnov: ", pasa_test)
 
-def randu():
-  generador = randu_generator()
-  print(generador.__repr__())
-  print(generador.next())
-  print(generador.next_float())
-  print(generador.next_float_range(4, 5))
-  print(generador.next_int_range(100, 200))
-  print(generador)
-  print(generador.valores)
-
-def mid_square():
-  generador = mid_square_generator(1931)
-  print(generador.__repr__())
-  print(generador.next())
-  print(generador.next())
-  print(generador.next())
-  print(generador.next())
-  print(generador)
-  print(generador.valores)
+def grafica(p1, p2, p3):
+    fig, axs = plt.subplots()
+    plt.figure(figsize=(10, 6))
+    plt.scatter(range(len(sec1)), sec1, label='GCL', color='blue', alpha=0.5)
+    plt.scatter(range(len(sec2)), sec2, label='Medios Cuadrados', color='red', alpha=0.5)
+    plt.scatter(range(len(sec3)), sec3, label='Numpy', color='green', alpha=0.5)
+    plt.xlabel('Índice')
+    plt.ylabel('Número Pseudoaleatorio')
+    plt.title('Gráfico de Dispersión de Números Pseudoaleatorios')
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
-  main()
+  sec1 = genCL(1000)
+  test_media(sec1)
+  test_chi_cuadrado(sec1)
+  test_autocorrelacion(sec1)
+  test_kolmogorov_smirnov(sec1)
+  sec2 = genCuad(1000)
+  test_media(sec2)
+  test_chi_cuadrado(sec2)
+  test_autocorrelacion(sec2)
+  test_kolmogorov_smirnov(sec2)
+  sec3 = np.random.rand(1000)
+  grafica(sec1, sec2, sec3)
